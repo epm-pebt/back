3d93f2d5cbabeabae715235fdca0af8849355ea8
Laszlo_Kosasvari@epam.com <Laszlo_Kosasvari@epam.com>
2024-02-27 15:59:03 +0100

    Sets up db environment variables with h2

--- src/main/java/com/epam/ecobites/config/SecurityConfig.java
+++ src/main/java/com/epam/ecobites/config/SecurityConfig.java
@@ -1,10 +1,14 @@
 package com.epam.ecobites.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +16,17 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http
-          .authorizeHttpRequests(request -> request
-              .requestMatchers( "/**").permitAll()
-          );
 
-      return http.build();
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        http
+          .authorizeHttpRequests(request -> request
+              .requestMatchers(
+                      new AntPathRequestMatcher("/h2-console/**"),
+                      new AntPathRequestMatcher("/**")
+              ).permitAll()
+          );
+        http.headers( header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
+        return http.build();
     }
 }
