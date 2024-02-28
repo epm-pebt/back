3ba50a915c4d888f9b67dfa3759b9553065bd193
Bernard_Haydu <bernard_haydu@epam.com>
2024-02-28 09:03:58 +0100

    0.04 - Configures in-memory H2 db, hides postgres credentials

--- src/main/java/com/epam/ecobites/config/SecurityConfig.java
+++ src/main/java/com/epam/ecobites/config/SecurityConfig.java
@@ -4,7 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
@@ -12,11 +15,16 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-      http
-          .authorizeHttpRequests(request -> request
-              .requestMatchers( "/**").permitAll()
-          );
+        http.csrf(AbstractHttpConfigurer::disable);
 
+        http
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers(
+                                new AntPathRequestMatcher("/h2-console/**"),
+                                new AntPathRequestMatcher("/**")
+                        ).permitAll()
+                );
+        http.headers( header -> header.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable));
       return http.build();
     }
 }
