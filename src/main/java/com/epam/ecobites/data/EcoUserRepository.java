e2c88ff1769a9a6e50cbc63179adceaf4bd5b41e
Kosasvaril <125371413+Kosasvaril@users.noreply.github.com>
2024-03-18 10:49:56 +0100

    Update EcoUserRepository.java

--- src/main/java/com/epam/ecobites/data/EcoUserRepository.java
+++ src/main/java/com/epam/ecobites/data/EcoUserRepository.java
@@ -4,6 +4,9 @@ import com.epam.ecobites.domain.EcoUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EcoUserRepository extends JpaRepository<EcoUser, Long> {
+    Optional<EcoUser> findByUsername(String username);
 }
