85a6c5550003e22f3a36240926f0ffabf819d8c5
marton98 <marton_udvardy1@epam.com>
2024-03-27 11:48:40 +0100

    Add RecipeService,Impl,Test and Mapper,Dto

---
+++ src/main/java/com/epam/ecobites/domain/dto/RecipeDto.java
@@ -0,0 +1,15 @@
+package com.epam.ecobites.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RecipeDto {
+    private Long id;
+    private String name;
+    private int time;
+    private String image;
+}
