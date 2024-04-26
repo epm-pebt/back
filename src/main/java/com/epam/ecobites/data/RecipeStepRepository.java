1ddf9b3b1d9c9767fa0ba0173057a6b695de909f
Kosasvaril <125371413+Kosasvaril@users.noreply.github.com>
2024-04-26 11:33:05 +0200

    Adds prepartion details section service
    RecipeStepDto, new query in RecipeStepRepository, RecipeStepMapper

--- src/main/java/com/epam/ecobites/data/RecipeStepRepository.java
+++ src/main/java/com/epam/ecobites/data/RecipeStepRepository.java
@@ -3,6 +3,9 @@ package com.epam.ecobites.data;
 import com.epam.ecobites.domain.RecipeStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface RecipeStepRepository extends JpaRepository<RecipeStep, Long> {
-    // custom queries..
+    List<RecipeStep> findByRecipeID(Long recipeID);
 }
