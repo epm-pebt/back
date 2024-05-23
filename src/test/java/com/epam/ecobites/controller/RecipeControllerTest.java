ab9a4258020b5c7242f1509c14d03a03e1c9f46b
Szabolcs Papdi <szabolcs_papdi@epam.com>
2024-05-23 11:32:19 +0200

    Adding controller for detailed recipe.

--- src/test/java/com/epam/ecobites/controller/RecipeControllerTest.java
+++ src/test/java/com/epam/ecobites/controller/RecipeControllerTest.java
@@ -1,6 +1,12 @@
 package com.epam.ecobites.controller;
 
+import com.epam.ecobites.domain.dto.DetailedRecipeDto;
+import com.epam.ecobites.domain.dto.IngredientDetailDto;
+import com.epam.ecobites.domain.dto.IngredientDto;
 import com.epam.ecobites.domain.dto.RecipeDto;
+import com.epam.ecobites.domain.dto.RecipeIngredientDto;
+import com.epam.ecobites.service.DetailedRecipeService;
+import com.epam.ecobites.service.DetailedRecipeServiceImpl;
 import com.epam.ecobites.service.RecipeService;
 import com.epam.ecobites.service.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
@@ -19,19 +25,21 @@ class RecipeControllerTest {
 
     public RecipeController systemUnderTest;
     public RecipeService<RecipeDto> recipeService = Mockito.mock(RecipeServiceImpl.class);
+    public DetailedRecipeService detailedRecipeService = Mockito.mock(DetailedRecipeServiceImpl.class);
 
     @BeforeEach
     public void setup(){
-        this.systemUnderTest = new RecipeController((RecipeServiceImpl) this.recipeService);
+        this.systemUnderTest = new RecipeController((RecipeServiceImpl) this.recipeService,
+            (DetailedRecipeServiceImpl) this.detailedRecipeService);
     }
 
     @Test
     void testGetAllRecipes() {
         List<RecipeDto> recipeDtos = new ArrayList<>();
-        recipeDtos.add(new RecipeDto(0,"food1",30, "url1","LUNCH", "VEGAN"));
-        recipeDtos.add(new RecipeDto(1,"food2",40, "url2", "LUNCH", "VEGAN"));
-        recipeDtos.add(new RecipeDto(2,"food3",50, "url3", "LUNCH", "VEGAN"));
-        recipeDtos.add(new RecipeDto(3,"food4",10, "url4", "LUNCH","VEGAN"));
+        recipeDtos.add(new RecipeDto(0L,"food1",30, "url1","LUNCH", "VEGAN"));
+        recipeDtos.add(new RecipeDto(1L,"food2",40, "url2", "LUNCH", "VEGAN"));
+        recipeDtos.add(new RecipeDto(2L,"food3",50, "url3", "LUNCH", "VEGAN"));
+        recipeDtos.add(new RecipeDto(3L,"food4",10, "url4", "LUNCH","VEGAN"));
         ResponseEntity<List<RecipeDto>> result =
                 new ResponseEntity<>(recipeDtos, HttpStatusCode.valueOf(200));
 
@@ -43,14 +51,42 @@ class RecipeControllerTest {
     @Test
     void testSearchRecipes() {
         List<RecipeDto> recipes = new ArrayList<>();
-        recipes.add(new RecipeDto("food", 10, "url1"));
-        recipes.add(new RecipeDto("food", 20, "url2"));
-        recipes.add(new RecipeDto("food", 30, "url3"));
+        recipes.add(new RecipeDto(1L, "food", 10, "url1","LUNCH", "VEGAN"));
+        recipes.add(new RecipeDto(2L, "food", 20, "url2", "LUNCH", "VEGAN"));
+        recipes.add(new RecipeDto(3L, "food", 30, "url3", "LUNCH", "VEGAN"));
 
         when(recipeService.searchRecipes("food")).thenReturn(recipes);
 
         ResponseEntity<List<RecipeDto>> expected = new ResponseEntity<>(recipes, HttpStatusCode.valueOf(200));
         ResponseEntity<List<RecipeDto>> actual = systemUnderTest.searchRecipes("food");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void testGetTopDetails() {
+        // GIVEN
+        IngredientDto ingredientDto = new IngredientDto("testIngredientName");
+        IngredientDetailDto ingredientDetailDto = new IngredientDetailDto(1, "ml");
+        RecipeIngredientDto recipeIngredientDto = new RecipeIngredientDto(ingredientDto, ingredientDetailDto);
+        List<RecipeIngredientDto> recipeIngredientDtos = new ArrayList<>();
+        recipeIngredientDtos.add(recipeIngredientDto);
+
+        RecipeDto recipeDto = new RecipeDto(1L, "food", 10, "url1",
+            "LUNCH", "VEGAN");
+
+        DetailedRecipeDto expectedRecipeDetail = new DetailedRecipeDto();
+        expectedRecipeDetail.setRecipeDto(recipeDto);
+        expectedRecipeDetail.setRecipeIngredientDtos(recipeIngredientDtos);
+
+        ResponseEntity<DetailedRecipeDto> expectedResponse = new ResponseEntity<>(expectedRecipeDetail,
+            HttpStatusCode.valueOf(200));
+
+        Mockito.when(detailedRecipeService.getRecipeDetails("testRecipeName")).thenReturn(expectedRecipeDetail);
+
+        // WHEN
+        var actualResponse = systemUnderTest.getTopDetails("testRecipeName");
+
+        // THEN
+        assertEquals(expectedResponse, actualResponse);
     }
 }
